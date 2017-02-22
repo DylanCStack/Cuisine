c3cfe4ef9c13db4ed8909fa5dcc778ec5558c534
Dylan Stackhouse and Matt Kelley <dylan@>
2017-02-22 11:48:37 -0800

    add review class and tests including function for adding, retrieve and edit

---
+++ src/Review.php
@@ -0,0 +1,107 @@
+<?php
+
+    class Review
+    {
+        private $review;
+        private $score;
+        private $restaurant_id;
+        private $id;
+
+        function __construct($review, $score, $restaurant_id, $id=null){
+            $this->review=$review;
+            $this->score = $score;
+            $this->restaurant_id = $restaurant_id;
+            $this->id=$id;
+        }
+
+        function setReview($review){
+            $this->review = $review;
+        }
+
+        function getReview(){
+            return $this->review;
+        }
+
+        function setScore($score){
+            $this->score = $score;
+        }
+
+        function getScore(){
+            return $this->score;
+        }
+
+        function getRestaurantId(){
+            return $this->restaurant_id;
+        }
+
+        function getId(){
+            return $this->id;
+        }
+
+        function save(){
+            $GLOBALS['DB']->exec("INSERT INTO review (review, score, restaurant_id) VALUES ('{$this->review}', {$this->score}, {$this->restaurant_id})");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        function update($text_update, $score_update){
+            $GLOBALS['DB']->exec("UPDATE review SET review = '{$text_update}', score = {$score_update} WHERE id = {$this->getId()};");
+            $this->setReview($text_update);
+            $this->setScore($score_update);
+        }
+
+        static function getAll(){
+            $found_reviews = $GLOBALS['DB']->query("SELECT * FROM review;");
+            $reviews = array();
+            foreach ($found_reviews as $review) {
+                $review_text = $review['review'];
+                $review_score = $review['score'];
+                $review_restaurant_id = $review['restaurant_id'];
+                $review_id = $review['id'];
+                $new_review = new Review($review_text, $review_score, $review_restaurant_id, $review_id);
+                array_push($reviews, $new_review);
+            }
+            return $reviews;
+        }
+
+        static function getAllByRestaurant($search_id){
+            $found_reviews = $GLOBALS['DB']->query("SELECT * FROM review WHERE restaurant_id = {$search_id};");
+            $reviews = array();
+            foreach ($found_reviews as $review) {
+                $review_text = $review['review'];
+                $review_score = $review['score'];
+                $review_restaurant_id = $review['restaurant_id'];
+                $review_id = $review['id'];
+                $new_review = new Review($review_text, $review_score, $review_restaurant_id, $review_id);
+                array_push($reviews, $new_review);
+            }
+            return $reviews;
+        }
+
+        static function deleteAll(){
+            $GLOBALS['DB']->exec("DELETE FROM review;");
+        }
+
+        // static function find($search_id)
+        // {
+        //     $found_restaurants = Review::getAll();
+        //     $returned_restaurant = null;
+        //
+        //     foreach($found_restaurants as $restaurant)
+        //     {
+        //         $new_restaurant = $restaurant->getId();
+        //
+        //         if($search_id == $new_restaurant){
+        //             $returned_restaurant = $restaurant;
+        //         }
+        //
+        //     }
+        //     return $returned_restaurant;
+        // }
+
+    }
+
+
+
+
+
+?>
--- tests/ReviewTest.php
+++ tests/ReviewTest.php
@@ -1,6 +1,7 @@
 <?php
     require_once 'src/Restaurant.php';
     require_once 'src/Cuisine.php';
+    require_once 'src/Review.php';
     /**
     * @backupGlobals disabled
     * @backupStaticAttributes disabled
@@ -10,12 +11,13 @@
     $password = 'root';
     $DB = new PDO($server, $username, $password);
 
-    class RestaurantTest extends PHPUnit_Framework_TestCase
+    class ReviewTest extends PHPUnit_Framework_TestCase
     {
         protected function tearDown()
         {
             Restaurant::deleteAll();
             Cuisine::deleteAll();
+            Review::deleteAll();
         }
 
         function test_save()
@@ -33,12 +35,18 @@
             $restaurant_cuisine = $cuisine1->getId();
 
             $restaurant = new Restaurant($restaurant_name,$restaurant_address,$restaurant_website, $restaurant_phone, $restaurant_cuisine);
-            // Act
             $restaurant->save();
 
+            $review_text = "Test Test 1/5 poor service";
+            $score = 1;
+            $review_restaurant_id = $restaurant->getId();
+
+            $review = new Review($review_text, $score, $review_restaurant_id);
+            // Act
+            $review->save();
             //Assert
-            $result = Restaurant::getAll();
-            $this->assertEquals([$restaurant], $result);
+            $result = Review::getAll();
+            $this->assertEquals([$review], $result);
         }
         function test_get_by_cuisine()
         {
@@ -57,6 +65,14 @@
             $restaurant1 = new Restaurant($restaurant_name,$restaurant_address,$restaurant_website, $restaurant_phone, $restaurant_cuisine);
             $restaurant1->save();
 
+            $review1_text = "Test Test 1/5 poor service";
+            $score1 = 1;
+            $review1_restaurant_id = $restaurant1->getId();
+
+            $review1 = new Review($review1_text, $score1, $review1_restaurant_id);
+            $review1->save();
+
+
             $cuisine_type2 = "Chinese";
             $id2 = null;
             $cuisine2 = new Cuisine($cuisine_type2,$id);
@@ -67,13 +83,56 @@
             $restaurant2_website = "BambooExpress.ninja";
             $restaurant2_phone = "123-456-7890";
             $restaurant2_cuisine = $cuisine2->getId();
+
             $restaurant2 = new Restaurant($restaurant2_name,$restaurant2_address,$restaurant2_website, $restaurant2_phone, $restaurant2_cuisine);
             $restaurant2->save();
 
+            $review2_text = "Test Test 2/5 poor service";
+            $score2 = 2;
+            $review2_restaurant_id = $restaurant2->getId();
+
+            $review2 = new Review($review2_text, $score2, $review2_restaurant_id);
+            $review2->save();
+
             //Assert
-            $result = Restaurant::getAllByCuisine($cuisine1->getId());
-            $this->assertEquals([$restaurant1], $result);
+            $result = Review::getAllByRestaurant($restaurant1->getId());
+            $this->assertEquals([$review1], $result);
         }
 
+        function test_edit_review(){
+            // Arrange
+            $cuisine_type1 = 'Mexican';
+            $id=null;
+            $cuisine1 = new Cuisine($cuisine_type1, $id);
+            $cuisine1->save();
+
+            $restaurant_name = "Taco Bills";
+            $restaurant_address = "123 Four street. Portland, OR 97226";
+            $restaurant_website = "TacoBills.ninja";
+            $restaurant_phone = "123-456-7890";
+            $restaurant_cuisine = $cuisine1->getId();
+
+            $restaurant = new Restaurant($restaurant_name,$restaurant_address,$restaurant_website, $restaurant_phone, $restaurant_cuisine);
+            $restaurant->save();
+
+            $review_text = "Test Test 1/5 poor service";
+            $score = 1;
+            $review_restaurant_id = $restaurant->getId();
+
+            $review = new Review($review_text, $score, $review_restaurant_id);
+            $review->save();
+
+            $review_text_update = "NVM WE ALL GOOD";
+            $score_update = 5;
+
+
+            //Act
+            $review->update($review_text_update, $score_update);
+
+            //Assert
+            $this->assertEquals("NVM WE ALL GOOD", $review->getReview());
+        }
+
+        
     }
 ?>
